6ce271c4ab44b87b7e62cd1297abca39a4ed3bab
Gulugulgula <119163937+Gulugulgula@users.noreply.github.com>
2023-06-04 23:06:44 +0530

    Add files via upload

---
+++ Task2_Pattern Recognition (Easy).py
@@ -0,0 +1,29 @@
+# How the code works
+'''
+first we define the function so that it takes a string 
+as an input 
+we define a loop inside the function in such a way
+that if there is a pattern which is not longer than half the string, 
+then pattern Found is printed and
+then the no of repititions which is equal to the string's length// value of iteration no
+if a pattern is no found then, pattern not found is printed 
+'''
+
+def find_pattern(string):
+    n = len(string)
+    for i in range(1, n // 2 + 1):
+        pattern = string[:i]
+        repetitions = n // i
+        if pattern * repetitions + string[:n % i] == string:
+            return pattern, repetitions
+    return None, 0
+
+script = input("Enter a string: ")
+pattern, repetitions = find_pattern(script)
+
+if pattern is not None:
+    print("Pattern Found:", pattern)
+    print("Repetitions:", repetitions)
+else:
+    print("Pattern Not Found")
+
